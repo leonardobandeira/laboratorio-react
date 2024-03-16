7e18edda6fd149044c68a6840695d666c5b3627c
Leonardo Lucena <leonardolucena.cc@gmail.com>
2024-03-16 16:35:08 -0300

    feat: Comunicação de componentes pai e filho

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo="Bom dia" nome="Leonardo" />
+        <Pai nome="Paulo" sobrenome="Silva" />
     </div>
     , document.getElementById('root')
 )
