5d208a246399c859966e94c1d97472a3c5a148ae
Leonardo Lucena <leonardolucena.cc@gmail.com>
2024-03-16 16:57:24 -0300

    feat: Fazendo uma evolução na comunicação de pai e filho

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Pai nome="Paulo" sobrenome="Silva" />
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho sobrenome="Souza" />
+            <Filho nome="Pedro" />
+            <Filho nome="Carla" />
+        </Pai>
     </div>
     , document.getElementById('root')
 )
