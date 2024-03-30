8a4a2de17732b8b72c908a634173d9b870a514b5
Leonardo Lucena <leonardolucena.cc@gmail.com>
2024-03-30 01:07:39 -0300

    style: Ajusta rodapé

--- frontend/src/components/templete/Footer.jsx
+++ frontend/src/components/templete/Footer.jsx
@@ -3,5 +3,9 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger'></i> por <a href="mailto:lucena.lbd@gmail.com?subject=Vamos tomar café?">
+                <strong>Leonardo Lucena <i className='fa fa-send text-blue'></i></strong>
+            </a>
+        </span>
     </footer>
