b769ace7f53cc5835614eb123b094218620cf8ae
Leonardo Lucena <leonardolucena.cc@gmail.com>
2024-03-31 01:09:32 -0300

    style: Ajusta estética de alguns elementos

--- frontend/src/components/templete/Nav.jsx
+++ frontend/src/components/templete/Nav.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 export default props => 
     <aside className='navegacao'>
         <nav className='menu'>
-            <a href="#">
-                <i className="fa fa-home">Início</i>
+            <a href="/">
+                <i className="fa fa-home"></i> Início
             </a>
-            <a href="/users#">
-                <i className="fa fa-users">Início</i>
+            <a href="/users">
+                <i className="fa fa-users"></i> Usuários
             </a>
         </nav>
     </aside>
