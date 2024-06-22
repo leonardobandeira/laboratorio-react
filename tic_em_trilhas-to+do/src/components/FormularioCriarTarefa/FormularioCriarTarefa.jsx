7c1c2832b131e82f5fc1df52758d9d8f41bf1172
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-21 23:56:56 -0300

    style: Estilizando o formulário

--- tic_em_trilhas-to+do/src/components/FormularioCriarTarefa/FormularioCriarTarefa.jsx
+++ tic_em_trilhas-to+do/src/components/FormularioCriarTarefa/FormularioCriarTarefa.jsx
@@ -1,9 +1,14 @@
+import style from './FormularioCriarTarefa.module.css'
+
 import { Botao } from "../Botao/Botao"
 import { CampoTexto } from "../CampoTexto"
 
 const FormularioCriarTarefa = () => {
     return (
-        <form action="">
+        <form 
+            action=""
+            className={style.FormularioCriarTarefa}
+        >
             <CampoTexto />
             <Botao texto='+' />
         </form>
