545b7a9e6bfa3f347fcac9f6cd022f929631c707
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-21 23:54:13 -0300

    style: Configura o estilo do botão com constantes

--- tic_em_trilhas-to+do/src/components/Botao/Botao.jsx
+++ tic_em_trilhas-to+do/src/components/Botao/Botao.jsx
@@ -1,7 +1,14 @@
+import style from './Botao.module.css'
+import { TIPO_BOTAO } from './Constantes'
+
 const Botao = (props) => {
-    const { texto, tipo = 'primario', outrasProps } = props
+    const { texto, tipo = TIPO_BOTAO.PRIMARIO, outrasProps } = props
     return (
-        <button tipo={tipo} {...outrasProps}>
+        <button
+            className={style.Botao}
+            tipo={tipo}
+            {...outrasProps}
+        >
             {texto}
         </button>
     )
