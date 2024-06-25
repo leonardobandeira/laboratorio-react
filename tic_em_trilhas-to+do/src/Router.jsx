eda4a176373b551bf51595cd7564ec948d9775f7
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-25 15:42:02 -0300

    feat: Configura rota padrão de erro e ajusta css da página de rotas

--- tic_em_trilhas-to+do/src/Router.jsx
+++ tic_em_trilhas-to+do/src/Router.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import { Inicial } from "./pages/Inicial";
 import { SobreNos } from "./pages/SobreNos";
 import { LayoutPadrao } from "./Layouts/LayoutPadrao/LayoutPadrao";
+import { NotFound } from "./pages/NotFound/NotFound";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<LayoutPadrao/>}>
+      <Route path="/" element={<LayoutPadrao />}>
         <Route path="/" element={<Inicial />} />
         <Route path="/sobre-nos" element={<SobreNos />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
