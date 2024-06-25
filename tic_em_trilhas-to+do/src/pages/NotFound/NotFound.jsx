77d8695e8d1b8f4e2c4658145c74e59da86ed1ad
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-25 15:32:47 -0300

    feat: Adiciona página não encontrada

---
+++ tic_em_trilhas-to+do/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import style from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className={style.NotFound}>
+        <h1>404<br/>Página não encontrada</h1>
+      </div>
+    </>
+  );
+};
+export { NotFound };
