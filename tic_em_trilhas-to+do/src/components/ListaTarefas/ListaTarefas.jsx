3a1cf80d37070c7199715cac22b056b4b10bfaf3
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-25 10:20:12 -0300

    feat: Faz a lista de itens ser dinamica

--- tic_em_trilhas-to+do/src/components/ListaTarefas/ListaTarefas.jsx
+++ tic_em_trilhas-to+do/src/components/ListaTarefas/ListaTarefas.jsx
@@ -1,12 +1,15 @@
-import style from './ListaTarefas.module.css'
-import { ListaTarefasItem } from './ListaTarefasItem/ListaTarefasItem';
+import style from "./ListaTarefas.module.css";
+import { ListaTarefasItem } from "./ListaTarefasItem/ListaTarefasItem";
 
 const ListaTarefas = () => {
+  const lista = [
+    { id: 1, nome: "Item 1" },
+    { id: 2, nome: "Item 2" },
+    { id: 3, nome: "Item 3" },
+  ];
   return (
     <ul className={style.ListaTarefa}>
-      <ListaTarefasItem nome="Item 1"/>
-      <ListaTarefasItem nome="Item 2"/>
-      <ListaTarefasItem nome="Item 3"/>
+      {lista.map( item => <ListaTarefasItem key={item.id} nome={item.nome}/>)}
     </ul>
   );
 };
